f7bd48d71b34061d05931bbc87587e1ab34ebf55
Farah Quader <fyquader@gmail.com>
2018-09-20 15:43:08 -0700

    Add passport.js library

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+passport.use(new GoogleStrategy());
+//clientid 538652116873-t2s3a7b24iq9r02be70jpmmn0pgnsqr1.apps.googleusercontent.com
 
 const PORT = process.env.PORT || 5000; // use port 5000 during development
 app.listen(PORT);
