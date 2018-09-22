8f341ec4f25439020559f514f39852ded9693e39
Farah Quader <fyquader@gmail.com>
2018-09-21 20:08:25 -0700

    Add config folder

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
 passport.use(new GoogleStrategy());
-//clientid 538652116873-t2s3a7b24iq9r02be70jpmmn0pgnsqr1.apps.googleusercontent.com
 
 const PORT = process.env.PORT || 5000; // use port 5000 during development
 app.listen(PORT);
